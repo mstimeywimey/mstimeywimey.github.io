4e9616e5377eab53a4be4018ae0549ea01fce581
mstimeywimey <mstimeywimey@gmail.com>
2018-03-29 12:09:24 -0400

    responsive page elements

---
+++ assets/js/CopyrightInfo.js
@@ -0,0 +1,2 @@
+﻿//Copyright information - ONLY EDIT TO CHANGE YEAR
+var Copyright = "&copy; 1970 - The Elementary Teachers\' Federation of Ontario <br/> ETFO holds the copyright to all information on this website, except where specifically noted. <br/> All rights reserved.";
